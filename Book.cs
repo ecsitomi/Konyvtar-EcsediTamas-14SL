3e2c38790ba7dac445e35baeafb81bedaa1d903b
Ecsedi Tamás József <ecsi.hgh@gmail.com>
2024-03-03 07:31:44 +0100

    Add project files.

---
+++ Book.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KonyvtarAsztaliKonzolos
+{
+    internal class Book
+    {
+        string author;
+        int id;
+        int page_count;
+        int published_year;
+        string title;
+
+        public string Author { get; set; }
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Published_year { get; set; }
+        public int Page_count { get; set; }
+
+        public Book(string author, int id, int page_count, int published_year, string title) 
+        { 
+            Author = author;
+            Id = id;
+            Page_count = page_count;
+            Published_year = published_year;
+            Title = title;
+        }
+
+        public override string ToString()
+        {
+            return $"ID: {Id}, Author: {Author}, Title: {Title}, Published Year: {Published_year}, Page Count: {Page_count}";
+        }
+    }
+}
