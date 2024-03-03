540b5a19c1034a8b0f8eac4eeda057b05e5d47e9
Ecsedi Tamás József <ecsi.hgh@gmail.com>
2024-03-03 08:32:48 +0100

    kész a konzolos feladat

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,34 @@ namespace KonyvtarAsztaliKonzolos
     {
         static void Main(string[] args)
         {
+            List<Book> books = Beolvasas("books.sql");
+
+            Statisztika statisztika = Statisztika.AdatbazisBeolvasas(books);
+            statisztika.OtszazOldalas();
+            statisztika.VanE1950();
+            statisztika.LeghosszabbKonyv();
+            statisztika.LegtobbKonyv();
         }
+        static List<Book> Beolvasas(string fajl) //sql beolvasás
+        {
+            List<Book> books = new List<Book>();
+            using (StreamReader sr = new StreamReader(fajl))
+            {
+                string sor;
+                while ((sor = sr.ReadLine()) != null)
+                {
+                    string[] reszek = sor.Split(';');
+                    string author = reszek[0];
+                    int id = int.Parse(reszek[1]);
+                    string title = reszek[2];
+                    int publishedYear = int.Parse(reszek[3]);
+                    int pageCount = int.Parse(reszek[4]);
+
+                    books.Add(new Book(author, id, pageCount, publishedYear, title));
+                }
+            }
+            return books;
+        }
+
     }
 }
--- Statisztika.cs
+++ Statisztika.cs
@@ -40,5 +40,76 @@ namespace KonyvtarAsztaliKonzolos
             }
             return statisztika;
         }
+        //statisztikai feladatok alább
+        public void OtszazOldalas()
+        {
+            int darab = 0;
+            foreach (var book in books)
+            {
+                if (book.Page_count > 500)
+                {
+                    darab++;
+                }
+            }
+            Console.WriteLine($"Ötszáz oldalnál hosszabb könyvek száma: {darab}");
+        }
+        public void VanE1950()
+        {
+            int darab = 0;
+            foreach (var book in Books)
+            {
+                if (book.Published_year < 1950)
+                {
+                    darab++;
+                }
+            }
+            if (darab != 0)
+            {
+                Console.WriteLine("Van 1950-nél régebben kiadott könyv.");
+            }
+            else
+            {
+                Console.WriteLine("Nincs 1950-nél régebben kiadott könyv.");
+            }
+        }
+        public void LeghosszabbKonyv()
+        {
+            Book leghosszabb = Books[0]; //lista első eleme a leghosszabb először mert vele kezdünk
+            foreach (var book in Books)
+            {
+                if (book.Page_count > leghosszabb.Page_count)
+                {
+                    leghosszabb = book;
+                }
+            }
+            Console.WriteLine("A leghosszabb könyv adatai:");
+            Console.WriteLine(leghosszabb.ToString());
+        }
+        public void LegtobbKonyv()
+        {
+            Dictionary<string, int> szerzokEsKonyveik = new Dictionary<string, int>();
+            foreach (var book in Books)
+            {
+                if (szerzokEsKonyveik.ContainsKey(book.Author))
+                {
+                    szerzokEsKonyveik[book.Author]++;
+                }
+                else
+                {
+                    szerzokEsKonyveik[book.Author] = 1; //első találatkor kap egyet
+                }
+            }
+            string szerzo = null;
+            int konyvek = 0;
+            foreach (var adatok in szerzokEsKonyveik) 
+            { 
+                if (adatok.Value > konyvek)
+                {
+                    szerzo = adatok.Key;
+                    konyvek = adatok.Value;
+                }
+            }
+            Console.WriteLine($"A legtermékenyebb szerző: {szerzo}, akinek van {konyvek} könyve.");
+        }
     }
 }
